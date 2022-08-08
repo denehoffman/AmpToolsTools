6b602a42d8a3139358b1ad743cfda45444fe3adb
gjwei1999 <53264802+gjwei1999@users.noreply.github.com>
2022-08-08 15:31:58 -0400

    add scripts for Phi-Pi system

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 AMPTOOLS_HOME = os.environ["AMPTOOLS_HOME"]
 ROOT_HOME = os.environ["ROOTSYS"]
-SRC = "src/ampwrapper"
+SRC = "/home/jiaweigu/AmpToolsTools/src/ampwrapper"
 
 amptools_extension = Extension(
         name="FitResults",
@@ -40,7 +40,11 @@ setup(
              SRC + "/amptools-search",
              SRC + "/amptools-select-thrown-topology",
              SRC + "/amptools-study",
-             SRC + "/amptools-view-thrown-topologies"],
+             SRC + "/amptools-view-thrown-topologies",
+             SRC + "/amptools-PhiPi-cfg",
+             SRC + "/amptools-PhiPi-fit",
+             SRC + "/amptools-PhiPi-result",
+             SRC + "/amptools-PhiPi-plot"],
     install_requires=[
         'numpy',
         'cython',
